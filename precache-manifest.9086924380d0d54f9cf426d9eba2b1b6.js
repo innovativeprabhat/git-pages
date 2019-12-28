443753bc28783cc5a47ec72419e2cd217e5c972c
prabhat.prakusi@gmail.com <xpras@outlook.com>
2019-12-29 01:26:17 +0530

    Updates

---
+++ precache-manifest.9086924380d0d54f9cf426d9eba2b1b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71b9699849a9c9894541fb29587e32fa",
+    "url": "/git-pages/index.html"
+  },
+  {
+    "revision": "0532b8ef61eeb271ef67",
+    "url": "/git-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b49c545ea421d7adb2f2",
+    "url": "/git-pages/static/js/2.a479d729.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/git-pages/static/js/2.a479d729.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0532b8ef61eeb271ef67",
+    "url": "/git-pages/static/js/main.d29c79d9.chunk.js"
+  },
+  {
+    "revision": "acefe2a3def8edd4f6b6",
+    "url": "/git-pages/static/js/runtime-main.cf63edaa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/git-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
